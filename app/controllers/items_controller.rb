fd315e240dac6765c27d1e19539d2f1ada5a388d
guchi0916 <football091634@icloud.com>
2021-02-17 20:24:53 +0900

    edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
+
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -16,11 +18,22 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-    @item = Item.find(params[:id])
+  def show   
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
+
   def item_params
     params.require(:item).permit(:image, 
                                  :name, 
@@ -33,4 +46,9 @@ class ItemsController < ApplicationController
                                  :day_id
                                 ).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
