9884193f5a953f5f180b0706a793b501e53007ab
guchi0916 <football091634@icloud.com>
2021-03-17 22:34:37 +0900

    Update 20210227082440_create_addresses.rb

--- db/migrate/20210227082440_create_addresses.rb
+++ db/migrate/20210227082440_create_addresses.rb
@@ -6,6 +6,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string  :city,                null: false
       t.string  :address,             null: false
       t.string  :build_name
+      t.string  :phone_number,        null: false
       t.integer :purchase_id,         null: false, foreign_key: true
       t.timestamps
     end
