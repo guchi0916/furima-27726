d048b70f3ddb8e18eee9de9a6844791be4af096d
guchi0916 <football091634@icloud.com>
2021-03-24 21:46:10 +0900

    Update 20210124095358_create_items.rb

--- db/migrate/20210124095358_create_items.rb
+++ db/migrate/20210124095358_create_items.rb
@@ -9,7 +9,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :charge_delivery_fee_id,    null: false
       t.integer :prefecture_id,             null: false
       t.integer :day_id,                    null: false
-      t.integer :user_id,                   null: false、foreign_key: true
+      t.integer :user_id,                   null: false,foreign_key: true
       t.timestamps
     end
   end
